05d12f8636f82ebc25a1a90007b0aa8d8d7dfc65
Yawn93 <89013752+Yawn93@users.noreply.github.com>
2024-12-05 19:49:35 +0000

    changes made

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
+// Elements
 const scrollButton = document.getElementById("downloadButton");
 scrollButton.style.display = "none";
 
+const successMessage = document.getElementById("successMessage");
+successMessage.style.display = "none";
+
+const closeButton = document.getElementById("closeButton");
+
+const closeButtonNews = document.getElementById("closeButtonNews");
+
+const sucessmessageNews = document.getElementById("sucessmessageNews");
+sucessmessageNews.style.display = "none";
+
+// Show/hide download button on scroll
 window.addEventListener("scroll", () => {
   if (window.scrollY > 0) {
     scrollButton.style.display = "block";
@@ -9,7 +21,23 @@ window.addEventListener("scroll", () => {
   }
 });
 
-function closeMessage() {
-  let successMessage = document.getElementById("successMessage");
-  successMessage.style.display = "none";
-}
+// Form submission handler contact
+document.getElementById("myForm").addEventListener("submit", (event) => {
+  event.preventDefault();
+  successMessage.style.display = "block";
+});
+
+// Close button handler success message
+closeButton.addEventListener("click", () => {
+  successMessage.style.display = "none"; // Hide the success message
+});
+
+document.getElementById("newsForm").addEventListener("submit", (event) => {
+  event.preventDefault();
+  sucessmessageNews.style.display = "block";
+});
+
+// Close button handler success message
+closeButtonNews.addEventListener("click", () => {
+  sucessmessageNews.style.display = "none"; // Hide the success message
+});
